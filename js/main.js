f329471d65f11af1b0eb1d4256bae18192c39c4d
rebs-code <jack.rebussi@gmail.com>
2023-11-28 15:48:24 +0100

    feat: milestone 3 add todo with input & button

--- js/main.js
+++ js/main.js
@@ -11,12 +11,20 @@ createApp({
         { text: "Create a todo list", done: false },
         { text: "Learn HTML", done: true },
       ],
+      newTodo: "",
     };
   },
   methods: {
     deleteTodo(index) {
-      console.log("deleteTodo");
       this.todos.splice(index, 1);
+    },
+    addTodo() {
+        if (this.newTodo.trim("") === "") {
+            this.newTodo = "";
+            return;
+        }
+        this.todos.push({ text: this.newTodo, done: false });
+        this.newTodo = "";
     },
   },
 }).mount("#app");
